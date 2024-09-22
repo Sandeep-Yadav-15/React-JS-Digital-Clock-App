888eb155dd86ee430c18bf4609df4cfb7d77f046
Sandeep-Yadav-15 <sandeepyadav.15719@gmail.com>
2024-09-22 13:59:49 +0530

    Updates

---
+++ precache-manifest.d47f54241f74bcd6dea454dc3042512d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6934df002aab0eb4fb65db6db431f41",
+    "url": "/React-JS-Digital-Clock-App/index.html"
+  },
+  {
+    "revision": "4033d71e2767696cf34e",
+    "url": "/React-JS-Digital-Clock-App/static/css/main.52c1f28a.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/React-JS-Digital-Clock-App/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/React-JS-Digital-Clock-App/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "4033d71e2767696cf34e",
+    "url": "/React-JS-Digital-Clock-App/static/js/main.9fb5f44a.chunk.js"
+  },
+  {
+    "revision": "e5edf7ef20bbdebd9100",
+    "url": "/React-JS-Digital-Clock-App/static/js/runtime~main.1345da3c.js"
+  }
+]);
